08eec6bb25207c587bea4ac6508fd37ff7a8edf2
Mithu <142711288+Mithu3311@users.noreply.github.com>
2024-12-29 15:36:14 +0500

    updated code

---
+++ src/app/navbar/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { Button } from '../../components/ui/button'; // Import Button from Shadcn
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="bg-orange-500 p-4 flex items-center justify-between">
+      {/* Logo */}
+      <div className="text-white text-2xl font-bold">
+        Dynamic Blog By Mithu
+      </div>
+
+      {/* Navbar Links */}
+      <div className="space-x-4">
+        <Link href="/">
+          <Button variant="link" className="text-white hover:text-gray-200">
+            Home
+          </Button>
+        </Link>
+        <Link href="/blog">
+          <Button variant="link" className="text-white hover:text-gray-200">
+            Blog
+          </Button>
+        </Link>
+        <Link target='_blank' href="https://github.com/Mithu3311">
+          <Button variant="link" className=" text-white hover:text-gray-200" >
+            About
+          </Button>
+        </Link>
+      </div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,11 @@
-'use client'
-import Link from 'next/link';
-import { posts } from './data/posts';
 
 export default function Home() {
   return (
-    <div className="p-4">
-      <h1 className="text-4xl font-bold mb-6">Welcome to My Blog</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id} className="mb-4">
-            <Link href={`/posts/${post.id}`} className="text-xl font-semibold text-blue-500">
-              {post.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="p-4 flex flex-col items-center justify-center text-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-6 text-orange-500">Welcome to My Blog</h1>
+      <p className="text-lg mb-6 text-gray-700">
+        Explore our collection of blog posts and share your thoughts in the comments!
+      </p>
     </div>
   );
 }
